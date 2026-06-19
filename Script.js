3033c622a3e5b6554539c0bbceac4e6267fafb92
Aryansft <114464206+Aryansft1@users.noreply.github.com>
2026-06-19 19:44:59 +0330

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-// highlight active section effect (simple clean UX)
-const links = document.querySelectorAll("nav a");
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    links.forEach(l => l.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
